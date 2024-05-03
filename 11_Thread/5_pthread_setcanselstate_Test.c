be39155d262906ad0e3d413d3e1b3d9dab012afb
JetWen <killer500k@gmail.com>
2024-05-03 11:27:28 +0800

    Create Thread Study & Pthread Function Tests

---
+++ 11_Thread/5_pthread_setcanselstate_Test.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+static void * new_thread_start(void * arg)
+{
+    /* Set Thread as uncancelable */
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+
+    while(1)
+    {
+        printf("New Threa--Running\n");
+        sleep(2);
+    }
+    return(void *)0;
+}
+
+int main (void)
+{
+    pthread_t tid;
+    int ret = 0;
+    void * tret;
+
+    ret = pthread_create(&tid, NULL, new_thread_start, NULL);
+    if(ret)
+    {
+        printf(stderr, "Error %s\n", strerror(ret));
+        exit(-1);
+    }
+    sleep(1);
+
+    /* Terminated the thread */
+    ret = pthread_cancel(tid);
+    if(ret)
+    {
+        printf(stderr, "pthread_cancel Error %s\n", strerror(ret));
+        exit(-1);
+    }
+
+    ret = pthread_join(tid, &tret);
+    if(ret)
+    {
+        printf(stderr, "pthread_join Error %s\n", strerror(ret));
+        exit(-1);
+    }
+
+    printf("New Thread End ,code = %ld\n", (long)tret);
+
+    exit(0);
+}
